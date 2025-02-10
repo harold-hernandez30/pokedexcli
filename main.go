c240ee33c9ee6e673b6fe29e80b0deb22e343a45
Harold Hernandez <harold.hernandez@visma.com>
2025-02-10 16:59:06 +0100

    Working prompt for repl - takes command and prints first slice of the words

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		res := scanner.Scan()
+		userInput := scanner.Text()
+
+		if res {
+			commands := strings.Fields(strings.ToLower(userInput))
+			command := commands[0]
+			fmt.Printf("Your command was: %s\n", command)
+		} 
+	}
 }
 
 
