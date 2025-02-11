70a987f21ee05f96e925b9792b6bc0c8df882cce
Harold Hernandez <harold.hernandez@visma.com>
2025-02-11 08:39:07 +0100

    Implement registry and execution of commands
    help, exit - commands

--- main.go
+++ main.go
@@ -7,6 +7,28 @@ import (
 	"strings"
 )
 
+var commandsMap = map[string]cliCommand{
+	"exit": {
+		name: "exit",
+		description: "Exit the Pokedex",
+		callback: commandExit,
+	},
+	"help": {
+		name: "help",
+		description: "Displays a help message",
+		callback: commandHelp,
+	},
+}
+
+var registry = map[string]cliCommand{}
+
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+
 func main() {
 	
 	scanner := bufio.NewScanner(os.Stdin)
@@ -16,12 +38,42 @@ func main() {
 		res := scanner.Scan()
 		userInput := scanner.Text()
 
+		registry["exit"] = commandsMap["exit"]
+		registry["help"] = commandsMap["help"]
+
 		if res {
 			commands := strings.Fields(strings.ToLower(userInput))
 			command := commands[0]
-			fmt.Printf("Your command was: %s\n", command)
+
+			cli, ok := commandsMap[command]
+
+			if ok {
+				err := cli.callback()
+				if err != nil {
+					fmt.Printf("Error found: %v", err)
+				}
+			} else {
+				fmt.Println("Unknown command")
+			}
+
 		} 
 	}
+}
+
+func commandExit() error {
+	defer os.Exit(0)
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Print("Usage:\n\n")
+
+	for command, obj := range registry {
+		fmt.Printf("%v: %v\n", command, obj.description)
+	}
+	return nil
 }
 
 
